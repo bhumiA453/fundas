4178ab492d017537aa7aed6c15ca20075827b168
Pranesh Nande <pranesh.nande@brand-scapes.com>
2025-01-22 18:45:18 +0530

    pdf issue solve

--- gallery_2017.php
+++ gallery_2017.php
@@ -1,4 +1,5 @@
 <?php include('header.php') ?>
+ 
 	<section></section>
-    <iframe src="https://fundas50.brand-scapes.com/pdf/gallery_2017.pdf" width="100%" height="600px" frameborder="0"></iframe>
+<object data="gallery_2017.pdf" type="application/pdf" frameborder="0" width="100%" height="500px"><embed src="https://drive.google.com/file/d/192p6aikRZJ1V0jBmnXpL9EhIrgDrfqVE/preview?usp=sharing" width="100%" height="600px"/></object>
 <?php include('footer.php') ?>
