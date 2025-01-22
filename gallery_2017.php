438f3323b384823fb305625eb2066cdf7132ca8f
Pranesh Nande <pranesh.nande@brand-scapes.com>
2025-01-22 14:05:01 +0530

    pdf compressed

--- gallery_2017.php
+++ gallery_2017.php
@@ -1,7 +1,5 @@
 <?php include('header.php') ?>
 	<section></section>
-  <iframe src="pdf/gallery_2017.pdf" width="100%" height="600px" frameborder="0"></iframe>
-    
-  </object>
+    <iframe src="https://fundas50.brand-scapes.com/pdf/gallery_2017.pdf" width="100%" height="600px" frameborder="0"></iframe>
     
 <?php include('footer.php') ?>
