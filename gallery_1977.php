24287cf0d99f61548d393fb014e6de67328225e4
Pranesh Nande <pranesh.nande@brand-scapes.com>
2025-01-22 16:03:10 +0530

    1977 pdf added

---
+++ gallery_1977.php
@@ -0,0 +1,4 @@
+<?php include('header.php') ?>
+	<section></section>
+    <iframe src="https://fundas50.brand-scapes.com/pdf/gallery_1977.pdf" width="100%" height="600px" frameborder="0"></iframe>
+<?php include('footer.php') ?>
