64a9a68998b12cd6ae658cab93a7f524a0932612
Pranesh Nande <pranesh.nande@brand-scapes.com>
2025-01-22 18:54:11 +0530

    pdf compressed

--- gallery_1977.php
+++ gallery_1977.php
@@ -1,5 +1,5 @@
 <?php include('header.php') ?>
  
 	<section></section>
-<object data="gallery_2017.pdf" type="application/pdf" frameborder="0" width="100%" height="500px"><embed src="https://drive.google.com/file/d/1m5cV1L2OLbAtNfhJl2WgO1coyvq0IthB/preview?usp=sharing" width="100%" height="600px"/></object>
+<object data="gallery_2017.pdf" type="application/pdf" frameborder="0" width="100%" height="500px"><embed src="https://drive.google.com/file/d/1Q9X93MRvn9d7Zhnv3cR_IKmAFkhhQOb0/preview?usp=sharing" width="100%" height="600px"/></object>
 <?php include('footer.php') ?>
