3ca874c9fd7e98aca8235bc9a4488ddfbd8430a9
Pranesh Nande <pranesh.nande@brand-scapes.com>
2025-02-03 18:02:06 +0530

    new pdf added under 2017

--- gallery_1977.php
+++ gallery_1977.php
@@ -2,4 +2,6 @@
  
 	<section></section>
 <object data="gallery_2017.pdf" type="application/pdf" frameborder="0" width="100%" height="500px"><embed src="https://drive.google.com/file/d/1Q9X93MRvn9d7Zhnv3cR_IKmAFkhhQOb0/preview?usp=sharing" width="100%" height="600px"/></object>
+
+
 <?php include('footer.php') ?>
